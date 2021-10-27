87665ec74006360daada1d94e65ecbd4a823fef8
Kenyi Hancco <kenyi.hancco@cloudcomputing.com.pe>
2021-10-27 17:14:07 -0500

    Added database exercise

---
+++ Pdo_sql.php
@@ -0,0 +1,28 @@
+<?php
+
+    # Crear varaiable de conexion
+    class Conexion {
+        private $servidor = "localhost";
+        private $base_datos = "db_prueba";
+        private $usuario = "root";
+        private $contrasena = "";
+        private $con;
+
+        # Crear conexión
+        public function __construct() {
+            $establecerConexion = "mysql:hos".$this->servidor."dbname=".$this->base_datos."charset=utf8";
+
+            try {
+                $this->con = new PDO($establecerConexion, $this->usuario, $this->contrasena);
+                $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "Conexión de base datos exitosa";
+            } catch (Exception $e) {
+                $this->con = "Error";
+                echo "Error: " . $e->getMessage();
+            }
+        }
+    }
+    $con = new Conexion();
+    #echo "Salid de la conexio";
+
+?>
